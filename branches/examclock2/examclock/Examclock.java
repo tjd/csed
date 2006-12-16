3142663081ee1bec047d8e5ab2d159c2f130a956
tjdonaldson <tjdonaldson@e47773e8-5a24-0410-b34c-f5020ee5ef57>
2006-12-16 07:27:36 +0000

    finished draft of "hr min" display style; alignment and size still need some tweaking

--- branches/examclock2/examclock/Examclock.java
+++ branches/examclock2/examclock/Examclock.java
@@ -3,8 +3,10 @@ package examclock;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Toolkit;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -19,29 +21,29 @@ import javax.swing.SwingConstants;
 
 public class Examclock {
 
-	private static final Color PANEL_BG_COLOR = Color.WHITE;
+	public static final Color PANEL_BG_COLOR = Color.WHITE;
 
-	private static final int MSG_FONT_SIZE = 40;
+	public static final int MSG_FONT_SIZE = 40;
 
-	private static final Font MSG_FONT = new Font("Times", Font.PLAIN,
+	public static final Font MSG_FONT = new Font("Times", Font.PLAIN,
 			MSG_FONT_SIZE);
 
-	private static final int TIME_ELAPSED_FONT_SIZE = 20;
+	public static final int TIME_ELAPSED_FONT_SIZE = 20;
 
-	private static final Font TIME_ELAPSED_FONT = new Font("Helvetica",
+	public static final Font TIME_ELAPSED_FONT = new Font("Helvetica",
 			Font.BOLD, TIME_ELAPSED_FONT_SIZE);
 
-	private static final int TIME_REMAINING_FONT_SIZE = 240;
+	public static final int TIME_REMAINING_FONT_SIZE = 240;
 
-	private static final Font TIME_REMAINING_FONT = new Font("Helvetica",
+	public static final Font TIME_REMAINING_FONT = new Font("Helvetica",
 			Font.BOLD, TIME_REMAINING_FONT_SIZE);
 
-	private static final String FRAME_NAME = "Exam Clock";
+	public static final String FRAME_NAME = "Exam Clock";
 
-	private static final int PANEL_WIDTH = Toolkit.getDefaultToolkit()
+	public static final int PANEL_WIDTH = Toolkit.getDefaultToolkit()
 			.getScreenSize().width;
 
-	private static final int PANEL_HEIGHT = Toolkit.getDefaultToolkit()
+	public static final int PANEL_HEIGHT = Toolkit.getDefaultToolkit()
 			.getScreenSize().height / 2;
 
 	public static void main(String[] args) {
@@ -54,11 +56,27 @@ public class Examclock {
 		panel.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
 		panel.setBackground(PANEL_BG_COLOR);
 
-		// time remaining label
-		JLabel timeRemaining = new JLabel();
-		timeRemaining.setFont(TIME_REMAINING_FONT);
-		timeRemaining.setHorizontalAlignment(SwingConstants.CENTER);
-		panel.add(timeRemaining, BorderLayout.CENTER);
+		// // time remaining label
+		// JLabel timeRemaining = new JLabel();
+		// timeRemaining.setFont(TIME_REMAINING_FONT);
+		// timeRemaining.setHorizontalAlignment(SwingConstants.CENTER);
+		// // panel.add(timeRemaining, BorderLayout.CENTER);
+		//
+		// MultiLabel lm = new MultiLabel();
+		// lm.add(new ClockLabel("2"));
+		// lm.add(new ClockLabel("hrs ", TIME_REMAINING_FONT_SIZE - 40));
+		// lm.add(new ClockLabel("5"));
+		// lm.add(new ClockLabel("min", TIME_REMAINING_FONT_SIZE - 40));
+
+		TimePanel timeRemainingPanel = new TimePanel(exam.hoursRemaining(),
+				exam.minutesRemaining());
+
+		// JPanel msgPanel = new JPanel();
+		// msgPanel.setLayout(new FlowLayout());
+		// msgPanel.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+		// msgPanel.setBackground(PANEL_BG_COLOR);
+
+		panel.add(timeRemainingPanel);
 
 		// time elapsed label
 		JLabel timeElapsed = new JLabel();
@@ -74,8 +92,8 @@ public class Examclock {
 
 		// set up timers
 		Timer clockTimer = new Timer();
-		TimerTask clockUpdate = new UpdateClockTask(timeRemaining, timeElapsed,
-				exam);
+		TimerTask clockUpdate = new UpdateClockTask(timeRemainingPanel,
+				timeElapsed, exam);
 		clockTimer.scheduleAtFixedRate(clockUpdate, 0, 60 * 1000);
 
 		// Timer msgTimer = new Timer();
@@ -97,6 +115,101 @@ public class Examclock {
 		return new Exam(minutes);
 	}
 }
+
+class TimePanel extends JPanel {
+	private JLabel hoursLabel;
+
+	private JLabel minutesLabel;
+
+	private JLabel hrLabel;
+
+	private JLabel minLabel;
+
+	public TimePanel(long hours, long mins) {
+		this.hoursLabel = new ClockLabel("" + hours);
+		this.hrLabel = new ClockLabel("hr ",
+				Examclock.TIME_REMAINING_FONT_SIZE - 40);
+		this.minutesLabel = new ClockLabel("" + mins);
+		this.minLabel = new ClockLabel("min",
+				Examclock.TIME_REMAINING_FONT_SIZE - 40);
+
+		add(this.hoursLabel);
+		add(this.hrLabel);
+		add(this.minutesLabel);
+		add(this.minLabel);
+		setBackground(Examclock.PANEL_BG_COLOR);
+	}
+
+	public void updateTime(long mins) {
+		long h = mins / 60;
+		long m = mins % 60;
+		updateTime(h, m);
+	}
+	
+	public void updateTime(long hours, long mins) {
+		this.hoursLabel.setText("" + hours);
+		this.minutesLabel.setText("" + mins);
+	}
+
+	public void setFinished() {
+		this.removeAll();
+		add(new ClockLabel(UpdateClockTask.EXAM_FINISHED_MSG));
+	}
+}
+
+class ClockLabel extends JLabel {
+	public ClockLabel(String msg) {
+		this(msg, Examclock.TIME_REMAINING_FONT);
+	}
+
+	public ClockLabel(String msg, int size) {
+		this(msg, Examclock.TIME_REMAINING_FONT, size);
+	}
+
+	public ClockLabel(String msg, Font f) {
+		super(msg);
+		setFont(f);
+	}
+
+	public ClockLabel(String msg, Font f, int size) {
+		super(msg);
+		setFont(new Font(f.getFontName(), Font.BOLD, size));
+	}
+}
+
+// class MultiLabel {
+// private ArrayList<JLabel> seq;
+// private JPanel panel;
+//	
+// public MultiLabel() {
+// seq = new ArrayList<JLabel>();
+// panel = new JPanel();
+// panel.setLayout(new FlowLayout());
+// }
+//	
+// public void add(String msg, Font f) {
+// JLabel lbl = new JLabel(msg);
+// lbl.setFont(f);
+// add(lbl);
+// }
+//	
+// public JLabel getLabel(int i) {
+// return seq.get(i);
+// }
+//	
+// public void setLabel(int i, String msg) {
+// getLabel(i).setText(msg);
+// }
+//	
+// public void add(JLabel lbl) {
+// seq.add(lbl);
+// panel.add(lbl);
+// }
+//	
+// public JPanel getLabelPanel() {
+// return panel;
+// }
+// }
 
 class AdviceGiver extends TimerTask {
 	private static final int FINAL_MSG_TIME = 10;
@@ -138,7 +251,7 @@ class AdviceGiver extends TimerTask {
 
 	@Override
 	public void run() {
-		if (exam.finished()) {
+		if (exam.isFinished()) {
 			adviceLabel.setText(EXAM_OVER_MSG);
 		} else {
 			if (exam.minutesRemaining() <= FINAL_MSG_TIME) {
@@ -151,22 +264,27 @@ class AdviceGiver extends TimerTask {
 }
 
 class UpdateClockTask extends TimerTask {
-	private static final String EXAM_FINISHED_MSG = "Time's up!";
+	public static final String EXAM_FINISHED_MSG = "Time's up!";
 
 	private static final Color LAST_TEN_MINUTES_COLOR = Color.ORANGE;
 
 	private static final Color LAST_MINUTE_COLOR = Color.RED;
 
-	private JLabel timeRemaining;
+	private TimePanel timeRemaining;
 
 	private JLabel timeElapsed;
 
 	private Exam exam;
 
-	public UpdateClockTask(JLabel timeRemaining, JLabel timeElapsed, Exam exam) {
+	public UpdateClockTask(TimePanel timeRemaining, JLabel timeElapsed,
+			Exam exam) {
 		this.timeRemaining = timeRemaining;
 		this.timeElapsed = timeElapsed;
 		this.exam = exam;
+	}
+
+	public static String sIf1(long n) {
+		return (n == 1) ? "" : "s";
 	}
 
 	public static String minToTime(long min) {
@@ -178,7 +296,7 @@ class UpdateClockTask extends TimerTask {
 		if (h < 1) {
 			return String.format("%s min", m);
 		} else if (m == 0) {
-			return String.format("%s hour%s", h, (h == 1) ? "" : "s");
+			return String.format("%s hour%s", h, sIf1(h));
 		} else {
 			return String.format("%shr %smin", h, m);
 		}
@@ -189,28 +307,30 @@ class UpdateClockTask extends TimerTask {
 		final long remaining = exam.minutesRemaining();
 		final long elapsed = exam.minutesElapsed();
 
-		if (exam.lastMinute()) {
+		if (exam.isLastMinute()) {
 			timeRemaining.setForeground(LAST_MINUTE_COLOR);
-		} else if (exam.lastTenMinutes()) {
+		} else if (exam.isLastTenMinutes()) {
 			timeRemaining.setForeground(LAST_TEN_MINUTES_COLOR);
 		}
 
-		if (exam.finished()) {
-			timeRemaining.setText(String.format(EXAM_FINISHED_MSG));
+		if (exam.isFinished()) {
+			timeRemaining.setFinished();
+			// timeRemaining.setText(String.format(EXAM_FINISHED_MSG));
 		} else {
-			timeRemaining.setText(minToTime(remaining));
+			timeRemaining.updateTime(remaining);
+			// timeRemaining.setText(minToTime(remaining));
 		}
 
 		timeElapsed.setText(String.format("%s minute%s elapsed", elapsed,
-				(elapsed == 1) ? "" : "s"));
+				sIf1(elapsed)));
 	}
 }
 
-enum ExamState {
-	running, lastTenMinutes, lastMinute, finished
-}
-
 class Exam {
+	enum StateEnum {
+		running, lastTenMinutes, lastMinute, finished
+	}
+
 	private final Calendar startTime;
 
 	private final Calendar endTime;
@@ -219,7 +339,7 @@ class Exam {
 
 	private final int durInSeconds;
 
-	private ExamState state;
+	private StateEnum currentState;
 
 	public Exam(int mins) {
 		this(0, mins);
@@ -233,7 +353,7 @@ class Exam {
 		this.endTime.add(Calendar.HOUR, hours);
 		this.endTime.add(Calendar.MINUTE, mins);
 		endTimeMillis = endTime.getTime().getTime();
-		state = ExamState.running;
+		currentState = StateEnum.running;
 	}
 
 	public String toString() {
@@ -242,7 +362,7 @@ class Exam {
 				.getTime()));
 		result.append(String.format("End time: %s\n", this.endTime.getTime()));
 		result.append(String.format("Duration: %ss \n", durInSeconds));
-		result.append(String.format("State: %s\n", this.state));
+		result.append(String.format("State: %s\n", this.currentState));
 		return result.toString();
 	}
 
@@ -290,25 +410,26 @@ class Exam {
 		return getDurInSeconds() - secondsRemaining();
 	}
 
-	public boolean lastTenMinutes() {
-		if (state != ExamState.lastTenMinutes && minutesRemaining() <= 10) {
-			state = ExamState.lastTenMinutes;
+	public boolean isLastTenMinutes() {
+		if (currentState != StateEnum.lastTenMinutes
+				&& minutesRemaining() <= 10) {
+			currentState = StateEnum.lastTenMinutes;
 		}
-		return state == ExamState.lastTenMinutes;
+		return currentState == StateEnum.lastTenMinutes;
 	}
 
-	public boolean lastMinute() {
-		if (state != ExamState.lastMinute && minutesRemaining() <= 1) {
-			state = ExamState.lastMinute;
+	public boolean isLastMinute() {
+		if (currentState != StateEnum.lastMinute && minutesRemaining() <= 1) {
+			currentState = StateEnum.lastMinute;
 		}
-		return state == ExamState.lastMinute;
+		return currentState == StateEnum.lastMinute;
 	}
 
-	public boolean finished() {
-		if (state != ExamState.finished && secondsRemaining() <= 1) {
-			state = ExamState.finished;
+	public boolean isFinished() {
+		if (currentState != StateEnum.finished && secondsRemaining() <= 1) {
+			currentState = StateEnum.finished;
 		}
-		return state == ExamState.finished;
+		return currentState == StateEnum.finished;
 	}
 
 	public Date getStartTime() {
