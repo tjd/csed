99c8d4acc7671cdb35c4a1788726cb3dca333893
tjdonaldson <tjdonaldson@e47773e8-5a24-0410-b34c-f5020ee5ef57>
2007-01-13 19:42:25 +0000

    added lotto example

---
+++ trunk/125/lotto/LottoPicker.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/* 
+ * In this lottery, 6 numbers (with no duplicates) are chosen in the range to 49 inclusive.
+ * 
+ */
+
+public class LottoPicker {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Lotto Picker");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel primary = new LottoPanel();
+		
+		frame.getContentPane().add(primary);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+}
