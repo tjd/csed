de3fe7ab1214f77682594f38d8e6a03629e13c3e
tjdonaldson <tjdonaldson@e47773e8-5a24-0410-b34c-f5020ee5ef57>
2007-01-13 01:23:18 +0000

    cleaned up code

--- asteroids/AsteroidsSprite.java
+++ asteroids/AsteroidsSprite.java
@@ -59,20 +59,26 @@ public class AsteroidsSprite {
 		// wrapped around to the other side.
 
 		this.angle += this.deltaAngle;
-		if (this.angle < 0)
+		if (this.angle < 0) {
 			this.angle += 2 * Math.PI;
-		if (this.angle > 2 * Math.PI)
+		}
+		if (this.angle > 2 * Math.PI) {
 			this.angle -= 2 * Math.PI;
+		}
 		this.currentX += this.deltaX;
-		if (this.currentX < -width / 2)
+		if (this.currentX < -width / 2) {
 			this.currentX += width;
-		if (this.currentX > width / 2)
+		}
+		if (this.currentX > width / 2) {
 			this.currentX -= width;
+		}
 		this.currentY -= this.deltaY;
-		if (this.currentY < -height / 2)
+		if (this.currentY < -height / 2) {
 			this.currentY += height;
-		if (this.currentY > height / 2)
+		}
+		if (this.currentY > height / 2) {
 			this.currentY -= height;
+		}
 	}
 
 	public void render() {
@@ -81,7 +87,7 @@ public class AsteroidsSprite {
 		// it's proper screen position.
 
 		this.sprite = new Polygon();
-		for (int i = 0; i < this.shape.npoints; i++)
+		for (int i = 0; i < this.shape.npoints; i++) {
 			this.sprite.addPoint((int) Math.round(this.shape.xpoints[i]
 					* Math.cos(this.angle) + this.shape.ypoints[i]
 					* Math.sin(this.angle))
@@ -89,6 +95,7 @@ public class AsteroidsSprite {
 					.round(this.shape.ypoints[i] * Math.cos(this.angle)
 							- this.shape.xpoints[i] * Math.sin(this.angle))
 					+ (int) Math.round(this.currentY) + height / 2);
+		}
 	}
 
 	public boolean isColliding(AsteroidsSprite s) {
@@ -96,12 +103,16 @@ public class AsteroidsSprite {
 		// Determine if one sprite overlaps with another, i.e., if any vertice
 		// of one sprite lands inside the other.
 
-		for (int i = 0; i < s.sprite.npoints; i++)
-			if (this.sprite.contains(s.sprite.xpoints[i], s.sprite.ypoints[i]))
+		for (int i = 0; i < s.sprite.npoints; i++) {
+			if (this.sprite.contains(s.sprite.xpoints[i], s.sprite.ypoints[i])) {
 				return true;
-		for (int i = 0; i < this.sprite.npoints; i++)
-			if (s.sprite.contains(this.sprite.xpoints[i], this.sprite.ypoints[i]))
+			}
+		}
+		for (int i = 0; i < this.sprite.npoints; i++) {
+			if (s.sprite.contains(this.sprite.xpoints[i], this.sprite.ypoints[i])) {
 				return true;
+			}
+		}
 		return false;
 	}
 }
