aae05f62940528c57ad3e193e9c24a2adbef5d9c
tjdonaldson <tjdonaldson@e47773e8-5a24-0410-b34c-f5020ee5ef57>
2007-01-26 03:25:01 +0000

    added random dithering example

--- 125spring2007/grayscale/RandomDither.java
+++ 125spring2007/grayscale/RandomDither.java
@@ -7,41 +7,21 @@ import util.EasyInput;
 import csimage.UberImage;
 import csimage.show;
 
-public class Grayscale {
+public class RandomDither {
 
 	public static void main(String[] args) {
 		UberImage image = UberImage.fromFile(EasyInput.chooseFile());
-		UberImage copy = new UberImage(image);
-		System.out.print("Calling makeBlackAndWhite ... ");
-		makeBlackAndWhite(image);
-		System.out.println("done");
+		bwRandomDither(image);
 		show.inFrame(image);
-		System.out.print("Calling bwRandomDither ...");
-		bwRandomDither(copy);
-		System.out.println("done");
-		show.inFrame(copy);
 	}
 
-	public static void makeBlackAndWhite(UberImage image) {
-		for (int i = 0; i < image.getWidth(); ++i) {
-			for (int j = 0; j < image.getHeight(); ++j) {
-				Color c = image.getColor(i, j);
-				if (brightness(c) < 3 * 255 / 2) {
-					image.setColor(i, j, Color.BLACK);
-				} else {
-					image.setColor(i, j, Color.WHITE);
-				}
-			}
-		}
-	}
-	
 	public static Random rnd = new Random();
 	
 	public static void bwRandomDither(UberImage image) {
 		for (int i = 0; i < image.getWidth(); ++i) {
 			for (int j = 0; j < image.getHeight(); ++j) {
 				Color c = image.getColor(i, j);
-				int r = rnd.nextInt(300);
+				int r = rnd.nextInt(256);
 				if (brightness(c) < r) {
 					image.setColor(i, j, Color.BLACK);
 				} else {
