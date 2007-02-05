d7eb48ce2dcebf457fdb4244e790399a4f80da26
tjdonaldson <tjdonaldson@e47773e8-5a24-0410-b34c-f5020ee5ef57>
2007-02-05 20:26:37 +0000

    added towers of Hanoi example

---
+++ 125spring2007/hanoi/TowerDemo.java
@@ -0,0 +1,14 @@
+package hanoi;
+
+public class TowerDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		TowersOfHanoi tower = new TowersOfHanoi(8);
+		tower.solve();
+
+	}
+
+}
