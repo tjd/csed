e478c0fc0feac39b8498aabd20f2579c42ad6d38
tjdonaldson <tjdonaldson@e47773e8-5a24-0410-b34c-f5020ee5ef57>
2007-02-14 00:29:27 +0000

    added recursion lab code

---
+++ trunk/125spring2007/recrusionlab/RecursionTest.java
@@ -0,0 +1,32 @@
+package recrusionlab;
+
+import myarraylist.List;
+
+
+public class RecursionTest {
+	public static void main(String[] args) {
+		util.Util.ensureAssertionsEnabled();
+		lengthTest();
+		sumTest();
+		System.out.println("all recursion tests passed");
+	}
+
+	public static void lengthTest() {
+		List<Integer> arr = new List<Integer>();
+		Recursion.
+		assert Recursion.length(arr) == 0;
+		arr.add(3);
+		assert Recursion.length(arr) == 1;
+		arr.add(4);
+		assert Recursion.length(arr) == 2;
+	}
+
+	public static void sumTest() {
+		List<Integer> arr = new List<Integer>();
+		assert Recursion.sum(arr) == 0;
+		arr.add(3);
+		assert Recursion.sum(arr) == 3;
+		arr.add(4);
+		assert Recursion.sum(arr) == 7;
+	}
+}
