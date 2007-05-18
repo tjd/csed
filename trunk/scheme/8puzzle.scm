4cbe9180ccef2e0e84ad228563fd0d66dd75f500
tjdonaldson <tjdonaldson@e47773e8-5a24-0410-b34c-f5020ee5ef57>
2007-05-18 15:43:01 +0000

    8-puzzle next-board working

--- trunk/scheme/8puzzle.scm
+++ trunk/scheme/8puzzle.scm
@@ -1,6 +1,6 @@
 ;;; 8puzzle.scm
 
-(load "tools.scm")
+(load "tools.scm") ;; need vector-copy
 
                                  ;; tile locations
 (define start-board '#(1 2 3     ;; 0 1 2
@@ -14,19 +14,22 @@
 (define (solved? board)
   (equal? board start-board))
 
+;;; returns a list of all possible boards that come from sliding the blank 1 location
 (define (next-boards board)
-  (let ((swap (lambda (i j) (swap-tiles i j board))))
-    (cond ((blank? (vector-ref board 0)) (list (swap 0 1) (swap 0 3)))
-          ((blank? (vector-ref board 1)) (list (swap 1 0) (swap 1 2) (swap 1 4)))
-          ((blank? (vector-ref board 2)) (list (swap 2 1) (swap 2 5)))
-          ((blank? (vector-ref board 3)) (list (swap 3 0) (swap 3 4) (swap 3 6)))
-          ((blank? (vector-ref board 4)) (list (swap 4 1) (swap 4 3) (swap 4 5) (swap 4 7)))
-          ((blank? (vector-ref board 5)) (list (swap 5 4) (swap 5 8)))
-          ((blank? (vector-ref board 6)) (list (swap 6 3) (swap 6 7)))
-          ((blank? (vector-ref board 7)) (list (swap 7 4) (swap 7 6) (swap 7 8)))
-          ((blank? (vector-ref board 8)) (list (swap 8 5) (swap 8 7)))
+  (let ((swap (lambda (i j) (swap-tiles i j board)))
+        (blank-at? (lambda (t) (blank? (vector-ref board t)))))
+    (cond ((blank-at? 0) (list (swap 0 1) (swap 0 3)))
+          ((blank-at? 1) (list (swap 1 0) (swap 1 2) (swap 1 4)))
+          ((blank-at? 2) (list (swap 2 1) (swap 2 5)))
+          ((blank-at? 3) (list (swap 3 0) (swap 3 4) (swap 3 6)))
+          ((blank-at? 4) (list (swap 4 1) (swap 4 3) (swap 4 5) (swap 4 7)))
+          ((blank-at? 5) (list (swap 5 4) (swap 5 8)))
+          ((blank-at? 6) (list (swap 6 3) (swap 6 7)))
+          ((blank-at? 7) (list (swap 7 4) (swap 7 6) (swap 7 8)))
+          ((blank-at? 8) (list (swap 8 5) (swap 8 7)))
           )))
-    
+
+;;; returns a new board with tiles at locations i and j swapped
 (define (swap-tiles i j board)
   (let ((new (vector-copy board)))
     (vector-set! new i (vector-ref board j))
