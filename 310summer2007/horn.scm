c0cc57fd0b84640e6ad0a2ed8a20e51b785c1cdf
tjdonaldson <tjdonaldson@e47773e8-5a24-0410-b34c-f5020ee5ef57>
2007-05-23 23:23:58 +0000

    added horn.scm

---
+++ 310summer2007/horn.scm
@@ -0,0 +1,29 @@
+;;; returns true of every item on lst satisfies test?
+(define (all? test? lst)
+  (if (null? lst) #t
+      (and (test? (car lst)) (all? test? (cdr lst)))))
+
+;;;
+;;; Horn clause recognizer functions
+;;; 
+(define (fact? x)
+  (and (list? x)
+       (= 1 (length x))
+       (symbol? (car x))))
+
+(define (integrity-constraint? x)
+  (and (list? x)
+       (< 1 (length x))
+       (all? symbol? x)))
+
+(define (definite-clause? x)
+  (and (list? x)
+       (<= 3 (length x))
+       (symbol? (car x))
+       (eq? '<-- (cadr x))
+       (all? symbol? (cddr x))))
+
+(define (horn-clause? x)
+  (or (fact? x)
+      (integrity-constraint? x)
+      (definite-clause? x)))
