cf641a2c2bf9d31ae0d535b9c01f878d3b78fd73
tjdonaldson <tjdonaldson@e47773e8-5a24-0410-b34c-f5020ee5ef57>
2007-05-25 00:15:49 +0000

    updated horn.scm

--- trunk/310summer2007/horn.scm
+++ trunk/310summer2007/horn.scm
@@ -4,13 +4,17 @@
       #t
       (and (test? (car lst)) (all? test? (cdr lst)))))
 
+;;; return a list containing just those elements that satisfy the test function
+(define (keep-if test-fn lst)
+  (cond ((null? lst) '())
+        ((test-fn (car lst)) (cons (car lst) (keep-if test-fn (cdr lst))))
+        (else (keep-if test-fn (cdr lst)))))
+
 ;;;
 ;;; Horn clause recognizer functions
 ;;; 
 (define (fact? x)
-  (and (list? x)
-       (= 1 (length x))
-       (symbol? (car x))))
+  (and (symbol? x) (not (eq? x '<--))))
 
 (define (integrity-constraint? x)
   (and (list? x)
@@ -28,3 +32,41 @@
   (or (fact? x)
       (integrity-constraint? x)
       (definite-clause? x)))
+
+;;; definite clause helper functions
+(define (head dc)
+  (car dc))
+
+(define (body dc)
+  (cddr dc))
+
+(define (premise dc)
+  (head dc))
+
+(define (conclusion dc)
+  (body dc))
+
+
+;;; sample KB: single fact
+(define KB-wet
+  '(wet))
+
+;;; sample KB: four facts and no clauses
+(define KB-pets
+  '(cat dog fish moose))
+
+;;; sample KB: one fact and one clause
+(define KB-awake
+  '(alarm
+    (awake <-- alarm)))
+
+;;; sample KB (from p. 220 of Norvig & Russell)
+(define KB-nr
+  '((Q <-- P)
+    (P <-- L M)
+    (M <-- B L)
+    (L <-- A P)
+    (L <-- A B)
+    A
+    B))
+
